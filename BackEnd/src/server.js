4209b6cea20c2dcd7af7ee29a9ab0d59118e924d
Saksham <sakshambansal2404@gmail.com>
2026-09-01 15:37:34 +0530

    Fix signup

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,14 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import path from 'path';
+import {connectDB} from './lib/db.js';
 
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
-app.use("api/auth", authRoutes);
-
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../FrontEnd/dist')));
     app.get('*', (_, res) => {
@@ -23,7 +24,7 @@ connectDB()
         console.log(`Server is running on port ${PORT}`);
     });
 })
-.catch((err => {
+.catch((err) => {
     console.error('Failed to connect to the database:', err);
     process.exit(1);
-}));
+});
