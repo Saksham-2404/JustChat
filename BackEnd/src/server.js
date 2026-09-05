c9cd44af5c695af29b1a0daff108e486305c4dc6
Saksham <sakshambansal2404@gmail.com>
2026-09-05 12:35:14 +0530

    message routes added

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from './routes/auth.js';
+import messageRoutes from './routes/message.js';
 import path from 'path';
 import {connectDB} from './lib/db.js';
 import {ENV} from './lib/env.js';
@@ -12,6 +13,7 @@ const PORT = ENV.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 if(ENV.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../FrontEnd/dist')));
     app.get('*', (_, res) => {
