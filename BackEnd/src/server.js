e499076eac713d4379bb04cb64c5950951c05321
Saksham <sakshambansal2404@gmail.com>
2026-09-06 15:21:18 +0530

    signup implemented on frontend

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -5,12 +5,14 @@ import path from 'path';
 import {connectDB} from './lib/db.js';
 import {ENV} from './lib/env.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = ENV.PORT;
 
 app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
---
+++ FrontEnd/src/components/PageLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { LoaderIcon } from 'lucide-react'
+function PageLoader() {
+  return (
+    <div className='flex items-center justify-center h-screen '>
+        <LoaderIcon className='w-12 h-12 text-blue-500 animate-spin' />
+    </div>
+  )
+}
+
+export default PageLoader
